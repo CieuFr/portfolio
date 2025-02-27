0ce2bf0984cd0bb3b0ab210ce063c377abbb7a25
Leiglat, Victor <victor.leiglat@thermofisher.com>
2025-02-27 15:24:59 +0100

    Adding base code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./pages/Home";
 import Resume from "./pages/Resume";
